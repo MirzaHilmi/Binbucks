fa829f13b43f1b1a514ae4eddf3e02b70f51a011
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2023-12-15 16:00:23 +0700

    ``feat``: Add Views implementation for every Books visited

--- models/Book.php
+++ models/Book.php
@@ -21,14 +21,21 @@ class Book {
     public bool $HardCopy;
     public bool $EBook;
     public bool $AudioBook;
+    public int $Views;
     public bool $Borrowed;
     public string $Bookshelf;
-    public \DateTime $CreatedAt;
-    public \DateTime $UpdatedAt;
+    public string $CreatedAt;
+    public string $UpdatedAt;
 
     /** @return Book[] */
     public static function fetchPartial(int $amount): array {
-        $query = "SELECT Books.*, Authors.Name AS Author FROM Books INNER JOIN Authors ON Books.AuthorID = Authors.ID ORDER BY RAND() LIMIT {$amount}";
+        $query = "
+        SELECT Books.*, Authors.Name AS Author
+        FROM Books
+        INNER JOIN Authors
+        ON Books.AuthorID = Authors.ID
+        ORDER BY RAND() LIMIT {$amount}";
+
         $result = MySQL::db()->query($query);
 
         $books = [];
@@ -37,5 +44,56 @@ class Book {
         }
 
         return $books;
+    }
+
+    public static function fetchByID(int $id): Book {
+        $query = '
+        SELECT Books.*, Authors.Name AS Author, Authors.Biography AS AuthorBio
+        FROM Books
+        INNER JOIN Authors
+        ON Books.AuthorID = Authors.ID
+        WHERE Books.ID = ?
+        LIMIT 1';
+
+        $stmt = MySQL::db()->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        $book = $stmt->get_result()->fetch_object(Book::class);
+        if ($book === null) {
+            throw new \Exception('Book Not Found');
+        }
+
+        return $book;
+    }
+
+    public static function getBorrowedTimes(int $id): int {
+        $query = '
+        SELECT COUNT(*) AS BorrowedTimes
+        FROM BorrowedBooks
+        WHERE BookID = ?
+        LIMIT 1';
+
+        $stmt = MySQL::db()->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result()->fetch_assoc();
+        if ($result === null) {
+            throw new \Exception('Book Not Found');
+        }
+
+        return $result['BorrowedTimes'];
+    }
+
+    public static function incrementViews(int $id): void {
+        $query = '
+        UPDATE Books
+        SET Views = Views + 1
+        WHERE ID = ?';
+
+        $stmt = MySQL::db()->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
     }
 }
